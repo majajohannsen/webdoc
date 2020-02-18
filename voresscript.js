6ce963879b500af70cf16c3baf888b9ab3392338
majajohannsen <johannsen12@hotmail.com>
2020-02-18 20:10:39 +0100

    version 18022020

--- voresscript.js
+++ voresscript.js
@@ -1,1 +1,53 @@
+"use strict";
 
+function erSynlig(elementId) {
+    const elementBoks = document.getElementById(elementId).getBoundingClientRect();
+    const halvtredsPct = elementBoks.height * 0.5;
+    const start = window.innerHeight - halvtredsPct;
+
+    if (elementBoks.top <= start && elementBoks.bottom - halvtredsPct > 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function aktiverTekstAnimation() {
+    for (let i = 0; i <= tekstIdListe.length - 1; i++) {
+        if (erSynlig(tekstIdListe[i])) {
+            document.getElementById(tekstIdListe[i]).classList.add("roll-in-right");
+        } else {
+            document.getElementById(tekstIdListe[i]).classList.remove("roll-in-right");
+        }
+    }
+}
+
+const tekstIdListe = ["tekst1", "tekst2", "tekst3", "tekst4"];
+
+
+
+function aktiverMultimedier() {
+    for (let i = 0; i <= AVIdListe.length - 1; i++) {
+        if (erSynlig(AVIdListe[i])) {
+            AVIndholdsliste[i].loop = true;
+            AVIndholdsliste[i].play();
+        } else {
+            AVIndholdsliste[i].pause();
+            AVIndholdsliste[i].currentTime = 0;
+        }
+    }
+}
+
+const AVIdListe = ["paris", "istanbul", "firenze", "video"];
+const AVIndholdsliste = [];
+
+AVIndholdsliste[0] = new Audio("Bouncy_Fun_1.mp3");
+AVIndholdsliste[1] = new Audio("Walker.mp3");
+AVIndholdsliste[2] = new Audio("Funky_Groove.mp3");
+AVIndholdsliste[3] = document.getElementById("video");
+
+window.addEventListener("scroll", function () {
+    aktiverTekstAnimation();
+    aktiverMultimedier();
+});
